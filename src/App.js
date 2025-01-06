72b3673ad13bb4d5b41615f1c6693bb47fc369c3
Yuka Kato <tappi_28@yahoo.co.jp>
2025-01-06 15:54:37 +0900

    課題1実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // React のインポート
+import { posts } from "./data/posts";
+import { ArticlesCard } from "./layout/ArticlesCard";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts.map((post) => (
+          <ArticlesCard
+            key={post.id}
+            title={post.title}
+            thumbnailUrl={post.thumbnailUrl}
+            createdAt={post.createdAt}
+            categories={post.categories}
+            content={post.content}
+          />
+        )
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header className="bg-zinc-700 font-bold text-white py-7 px-5">
+      <a href="/" className="">
+        Blog
+      </a>
+      <a href="/" className="float-right">
+        お問い合わせ
+      </a>
+    </header>
+  );
+};
