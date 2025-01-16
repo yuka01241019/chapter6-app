e857c75c7f0b480046c8782c8517bc0c6e1fc0b2
Yuka Kato <tappi_28@yahoo.co.jp>
2025-01-16 14:16:39 +0900

    Tailwindで修正

--- src/layout/ArticlesCard.js
+++ src/layout/ArticlesCard.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 export const ArticlesCard = ({ post, className }) => {
   // 日付表示を変更（YYYY/MM/DD形式）
   const date = new Date(post.createdAt).toLocaleDateString("ja-JP");
-  // 先頭3行のみを表示する関数を作成
-  const getFirstThreeLines = (content) => {
-    // splitメソッドで<br/>タグがある位置で区切る
-    const lines = content.split("<br/>");
-    // sliceメソッドで配列の0番目～2番目（3つ分）の要素を取得する
-    // joinメソッドで先頭3つの要素を再び<br/>で結合（改行の形を維持）
-    // 条件演算子を使って配列の要素の数が3つを超える場合（true）は、最後に「...」を追加する。
-    // 配列の要素数が3つ以下の場合（false）は、何も追加しない。
-    return lines.slice(0,3).join("<br/>") + (lines.length > 3 ? "<br/>..." : "");
-  };
+  // // 先頭3行のみを表示する関数を作成
+  // const getFirstThreeLines = (content) => {
+  //   // splitメソッドで<br/>タグがある位置で区切る
+  //   const lines = content.split("<br/>");
+  //   // sliceメソッドで配列の0番目～2番目（3つ分）の要素を取得する
+  //   // joinメソッドで先頭3つの要素を再び<br/>で結合（改行の形を維持）
+  //   // 条件演算子を使って配列の要素の数が3つを超える場合（true）は、最後に「...」を追加する。
+  //   // 配列の要素数が3つ以下の場合（false）は、何も追加しない。
+  //   return lines.slice(0,3).join("<br/>") + (lines.length > 3 ? "<br/>..." : "");
+  // };
   return (
     <Link
       to={`/posts/${post.id}`} //クリックするとid毎の詳細ページに飛ぶ
@@ -31,8 +31,8 @@ export const ArticlesCard = ({ post, className }) => {
       </div>
       <div className="clear-both text-left pt-4">{post.title}</div>
       <div
-        className="pt-4"
-        dangerouslySetInnerHTML={{ __html: getFirstThreeLines(post.content) }}
+        className="pt-4 line-clamp-3"
+        dangerouslySetInnerHTML={{ __html: post.content }}
       ></div>
     </Link>
   );
